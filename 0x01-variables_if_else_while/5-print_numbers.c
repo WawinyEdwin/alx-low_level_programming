09e4d39b184dccd926b51601a14948dc019091fa
WawinyEdwin <wawinyedwin44@gmail.com>
2022-09-09 06:52:23 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print single digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		printf("%d", k);
+	}
+	printf("\n");
+	return (0);
+}
