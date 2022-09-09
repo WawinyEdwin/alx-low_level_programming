12c7049be415e69ceccb82991cbb4d48f34f4feb
WawinyEdwin <wawinyedwin44@gmail.com>
2022-09-09 07:05:55 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Prints all single digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int k;
+
+	for (k = 0; k < 10; k++)
+	{
+		putchar(k + '0');
+	}
+	putchar('\n');
+	return (0);
+}
