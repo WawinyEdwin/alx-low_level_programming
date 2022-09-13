0a986682146b356955849d57dd59a9dcccafd091
WawinyEdwin <wawinyedwin44@gmail.com>
2022-09-13 09:26:56 -0700

    Correct putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print lowercase
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+/* prototypes of functions */
+
+#ifndef main.h
+#define main.h
+
+int _putchar(char c)
+
