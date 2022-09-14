19cbdc5996cf24dd037ccfdc25aa88ef2cc78b58
WawinyEdwin <wawinyedwin44@gmail.com>
2022-09-14 15:29:38 -0700

    Add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Sum
+ * Description: Add integers
+ * @i: first param
+ * @j: second param
+ * Return: int value
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j
+	return (sum);
+}
