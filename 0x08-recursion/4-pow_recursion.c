ecea096b4b303c2190aef7ce744dd68c05bb25f5
WawinyEdwin <wawinyedwin44@gmail.com>
2022-09-29 07:08:50 -0700

    power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - power
+ * @x: int
+ * @y: another
+ * Return: the poweer
+ */
+int _pow_recursion(int x, int y)
+{
+	int res = x;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+	return (result);
+}
